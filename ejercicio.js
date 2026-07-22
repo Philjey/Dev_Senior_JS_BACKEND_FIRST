d85f2ffe00f61add30fd1a8c4d1c36d1caa74047
Philjey <philjeyvg.27@gmail.com>
2026-07-22 00:22:42 -0500

    tercer intento

--- ejercicio.js
+++ ejercicio.js
@@ -1,4 +1,7 @@
 // este va es mi segundo  intento haciendolo sin pistas
 // primero es git init, luego git add ., luego va git commit -m, luego va git remote add origin -u main y luego va git push. 
 console.log ("Segundo intento");
-
+// bueno me equivoque porque ne el git commit se coloca el titulo del proyecto.
+// tambien me quivoque con el tema del git remote add origin, ya que arroja que ya xiste, asi que infiero que no hace falta colocarlo de nuevo
+// bueno aca no se porque se cambio el nombre de todo a segundo intento asi qeu tratare de arreglarlo en ejerciio_2.js
+// en efecto mi erro tambien fue no haber guardado la actualización. 
